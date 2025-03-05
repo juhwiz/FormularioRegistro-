9fc5fbd837d816d5f2750ad42d8a7c6038f750ff
Julia Simão de Almeida Cruz <62820555+juhwiz@users.noreply.github.com>
2025-03-04 22:52:07 -0300

    Update com editar usuário
    Possui um erro ao editar mais de um usuário

--- Projeto/script.js
+++ Projeto/script.js
@@ -3,44 +3,10 @@
 const form = document.getElementById('userForm');
 const listBtn = document.getElementById('listBtn');
 const userList = document.getElementById('usersList');
+const alterarBtn = document.getElementById('alterarBtn');
 
 //Array de usuários
 let users = [];
-
-//Adicionar usuários
-function addUser(name, age, course){
-    users.push({name, age, course});
-}
-
-//display de usuários
-function displayUsers(){
-    usersList.innerHTML = '';
-    users.forEach((user, index) => {
-        const userHTML = `        
-            <div class="userItem">
-            <h3>${user.name}</h3>
-            <p>Age: ${user.age}</p>
-            <p>Course: ${user.course}</p>
-            <button class="deleteBtn" onclick="deleteUser(${index})">Deletar</button>
-        </div>`;
-
-        usersList.insertAdjacentHTML('beforeend', userHTML);
-    });
-}
-
-//Function deletar
-function deleteUser(index){
-    users.splice(index, 1);
-    displayUsers();
-}
-
-//Function lista de usuarios 
-function toggleUsersList(){
-    usersList.classList.toggle('hidden');
-    if(!usersList.classList.contains('hidden')){
-        displayUsers();
-    }
-}
 
 //Formulário para inserir usuário 
 form.addEventListener('submit', (e) => {
@@ -54,6 +20,83 @@ form.addEventListener('submit', (e) => {
 
 });
 
+//Adicionar usuários
+function addUser(name, age, course){
+    users.push({name, age, course});
+    console.log(users);
+}
+
+//display de usuários
+function displayUsers(){
+    userList.innerHTML = '';
+    users.forEach((user, index) => {
+        const userHTML = `        
+            <div class="userItem">
+            <h3>${user.name}</h3>
+            <p>Age: ${user.age}</p>
+            <p>Course: ${user.course}</p>
+            <button class="editBtn" onclick="editUser(${index})">Editar</button>
+            <button class="deleteBtn" onclick="deleteUser(${index})">Deletar</button>
+        </div>`;
+
+        usersList.insertAdjacentHTML('beforeend', userHTML);
+        console.log(userHTML);
+    });
+}
+
+//Function deletar
+function deleteUser(index){
+    users.splice(index, 1);
+    displayUsers();
+}
+
+//Funtion editar usuário
+function editUser(index){
+
+    console.log("index antes do alterarBtn: " + index);
+
+    //Devolve valor para o input do form
+    document.getElementById('name').value = users[index].name;
+    document.getElementById('age').value = users[index].age;
+    document.getElementById('course').value = users[index].course;
+
+    document.getElementById('alterarBtn').style.display = "unset";
+    
+    //Alterar lista de usuário
+    alterarBtn.addEventListener('click', (e) => {
+        e.preventDefault();
+        
+        //Coleta os valores novos do input
+        var name = document.getElementById('name').value;
+        var age = document.getElementById('age').value;
+        var course = document.getElementById('course').value;
+
+        //Passa para o array
+        users[index].name = name;
+        users[index].age = age;
+        users[index].course = course;
+
+        //Recarrega a lista usuário e limpa o form
+        displayUsers();
+        form.reset();
+
+        document.getElementById('alterarBtn').style.display = "none";
+        console.log("index no final: " + index);
+        console.log(users);
+    });
+
+}
+
+
+//Function lista de usuarios 
+function toggleUsersList(){
+    usersList.classList.toggle('hidden');
+    if(!usersList.classList.contains('hidden')){
+        displayUsers();
+    }
+}
+
 //botão listagem 
 
 listBtn.addEventListener('click', toggleUsersList);
+
